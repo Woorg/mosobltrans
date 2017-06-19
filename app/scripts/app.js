e808b23aed84a5367fa30cc34b75d4479b0f8440
Igor Gorlov <gorlov35@gmail.com>
2017-06-19 22:53:56 +0300

    style categories

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,19 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import 'magnific-popup';
+import 'jquery-validation';
+import tabs from '../blocks/categories/categories.js';
 
 $(() => {
 	svg4everybody();
+
+	$(function () {
+
+
+		// Tabs
+		tabs();
+
+
+	});
+
 });
